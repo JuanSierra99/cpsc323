02b256197ccd3d10f80d31a9654eba6b91f0ee4d
jnazzario <98191405+jnazzario@users.noreply.github.com>
2023-02-01 23:57:53 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,79 +11,55 @@
 #include <string>
 #include <stack>
 
-// This is supposed to translate the characters input by the user into numerical values but it is not working :(
-int getValue(char c)
-{
-    switch (c) {
-        case 'a':
-            return 5;
-            break;
-        case 'b':
-            return 7;
-            break;
-        case 'c':
-            return 2;
-            break;
-        case 'd':
-            return 4;
-            break;
-            
-        default: return -1;
-            break;
-    }
-    
-}
 //---------------------------------------------------------------------------------------------------
     int postfixCalc(std::string expression)
 {
-        std::stack<int> stackStack;
+        std::stack<int> numStack;
 
-        for(int i = 0; i < expression.length(); i++)
+        for(int i = 0; i < expression.length()-1; i++)
         {
-            // Creates the stack of numbers only
-            if(isdigit(expression[i]))
-            {
-                int num = getValue(expression[i]);
-                stackStack.push(num);
-            }else
-            {
+            // Converts the string characters into numbers
+            if(expression[i] == 'a')
+                numStack.push(5);
+            else if (expression[i] == 'b')
+                numStack.push(7);
+            else if (expression[i] == 'c')
+                numStack.push(2);
+            else if (expression[i] =='d')
+                numStack.push(4);
+        else{
                 // Assigns the top of the stack into a variable to use it mathematically and then pops it
-                int num1 = stackStack.top();
-                stackStack.pop();
+                int num1 = numStack.top();
+                numStack.pop();
 
                 // Assigns the new top of the into a variable to use it mathematically and then pops it
-                int num2 = stackStack.top();
-                stackStack.pop();
+                int num2 = numStack.top();
+                numStack.pop();
                 
                 // makes the operator perform its operation
                 switch (expression[i]) {
                     case '+':
-                        stackStack.push(num1 + num2);
-                        break;
+                        numStack.push(num1 + num2); break;
                     case '-':
-                        stackStack.push(num1 - num2);
-                        break;
+                        numStack.push(num1 - num2); break;
                     case '*':
-                        stackStack.push(num1 * num2);
-                        break;
+                        numStack.push(num1 * num2); break;
                     case '/':
-                        stackStack.push(num1 / num2);
-                        break;
-                    case '$':
-                        break;
+                        numStack.push(num1 / num2); break;
+                    case '$': break;
                 }
             }
         }
 
-        return stackStack.top();
+        return numStack.top();
 }
 //---------------------------------------------------------------------------------------------------
 int main() {
     int result = 0;
     std::string expression;
-    std::string continuee;
+    std::string next;
     
-    while(continuee != "n")
+    while(next != "n")
     {
         // Input - gets the expression input by the user
         std::cout << "Enter a postfix expression with $ at the end: ";
@@ -96,7 +72,7 @@ int main() {
         std::cout << "\tValue = " << result << std::endl;
         
         std::cout << "CONTINUE(y/n)? ";
-        getline(std::cin, continuee);
+        getline(std::cin, next);
     }
     return 0;
 }
