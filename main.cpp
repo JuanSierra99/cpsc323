498d49f29e2b5e0c26bffe6dcb1ebb5d5576580a
jnazzario <98191405+jnazzario@users.noreply.github.com>
2023-02-07 19:45:47 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,24 @@
-//-------------------------------------------------------------------------------------------
-//  Group Names: 
+//---------------------------------------------------------------------------------------------------
+//  Group Name: Carlos Mora, Juan Sierra, Juli Nazzario
 //  Assignment: No 1
 //  Due Date: 2/2/2023
-//  Purpose: This program reads an expression in postfix form, evaluates the expression,
-//  and displays its value
-//-------------------------------------------------------------------------------------------
-
+//  Purpose: This program reads in a postfix expression, evaluates the expression, and displays its value
+//---------------------------------------------------------------------------------------------------
 #include <iostream>
 #include <istream>
 #include <string>
 #include <stack>
-
-//-------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------
     int postfixCalc(std::string expression)
 {
         std::stack<int> numStack;
-
         for(int i = 0; i < expression.length()-1; i++)
         {
             // Creates the stack of numbers only
-            if(expression[i] == 'a')
-                numStack.push(5);
-            else if (expression[i] == 'b')
-                numStack.push(7);
-            else if (expression[i] == 'c')
-                numStack.push(2);
-            else if (expression[i] =='d')
-                numStack.push(4);
+            if(expression[i] == 'a')       numStack.push(5);
+            else if (expression[i] == 'b') numStack.push(7);
+            else if (expression[i] == 'c') numStack.push(2);
+            else if (expression[i] == 'd') numStack.push(4);
         else{
                 // Assigns the top of the stack into a variable to use it mathematically and then pops it
                 int num1 = numStack.top();
@@ -46,20 +38,18 @@
                         numStack.push(num2 * num1); break;
                     case '/':
                         numStack.push(num2 / num1); break;
-                    case '$': break;
-                }
+                    case '$': break; }
             }
         }
-
         return numStack.top();
 }
-//-------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------
 int main() {
     int result = 0;
     std::string expression;
-    std::string continuee;
+    std::string next;
     
-    while(continuee != "n")
+    while(next != "n")
     {
         // Input - gets the expression input by the user
         std::cout << "Enter a postfix expression with $ at the end: ";
@@ -72,7 +62,7 @@ int main() {
         std::cout << "\tValue = " << result << std::endl;
         
         std::cout << "CONTINUE(y/n)? ";
-        getline(std::cin, continuee);
+        getline(std::cin, next);
     }
     return 0;
 }
